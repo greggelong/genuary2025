7f6f557912c6d9d0cdd2ec34c800bdaad0c08b85
Greg K <gkreisman_cep@qq.com>
2025-01-02 08:33:42 +0800

    first commit

---
+++ day2/sketch.js
@@ -0,0 +1,26 @@
+let hair
+let mytxt =["My","PRETTY","HAIR","HAIR","HAIR"]
+function preload(){
+  hair = loadImage("hair.png")
+}
+function setup() {
+  createCanvas(900, 900);
+  imageMode(CENTER)
+  textAlign(CENTER,CENTER)
+  hair.resize(200,0)
+  frameRate(7)
+}
+
+function draw() {
+  //background(220);
+  for(let i=0; i<20; i++){
+    image(hair,random(width),random(height))
+  }
+  
+  fill(0,255,0)
+  textFont('Courier New', random(40,142));
+  
+  for(let j=0;j<5;j++){
+    text(random(mytxt),random(width),random(height))
+  }
+}
