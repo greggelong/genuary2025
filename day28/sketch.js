3e6c0202c1e21ac76de4247e1abc02f28482b762
Greg K <gkreisman_cep@qq.com>
2025-01-27 21:42:18 +0100

    day 28

---
+++ day28/sketch.js
@@ -0,0 +1,55 @@
+let nx = 1000;
+let nxstart = 1000;
+let nystart = 1000;
+let ny = 1000;
+let sp = 0.03;
+let sz;
+let column = 100;
+let rows = 100;
+//let asciiChars = [' ', '.', ':', '-', '=', '+', '*', '#', '%', '@']; // ASCII gradient
+
+let asciiChars = [
+  "🐭",
+  "🐂",
+  "🐅",
+  "🐇",
+  "🐉",
+  "🐍",
+  "🐎",
+  "🐐",
+  "🐒",
+  "🐓",
+  "🐕",
+  "🐖",
+];
+let clr;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight); // Full-screen canvas
+  sz = width / column; // Adjust size based on width
+  background(0);
+  noStroke();
+  textSize(sz);
+  textFont("monospace");
+  frameRate(10);
+}
+
+function draw() {
+  background(0);
+  sp = map(mouseY, 0, height, -0.3, 0.3);
+  ny = nystart;
+  for (let j = 0; j < rows; j++) {
+    nx = nxstart; // reset noise x position
+    for (let i = 0; i < column; i++) {
+      clr = noise(nx, ny);
+      let charIndex =
+        asciiChars.length - 1 - floor(clr * (asciiChars.length - 1));
+      let asciiChar = asciiChars[charIndex];
+      fill(0); // White color for text
+      text(asciiChar, i * sz, j * sz + sz);
+      nx += 0.05;
+    }
+    ny += 0.05;
+  }
+  nystart += sp;
+}
